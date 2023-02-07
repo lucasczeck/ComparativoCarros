270601a89945397f52a8166693c6cbd2d903cac2
lucas <lucas.czeck@inteliger.com.br>
2023-02-06 22:59:36 -0300

    Adicionada página inicial de login ou cadastro

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
+    path('', views.Login.as_view(), name='login'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.views import View
 import BO.home.home
 
 
+class Login(View):
+
+    def get(self, *args, **kwargs):
+        context = {}
+
+        return render(self.request, "login.html", context=context)
