6ab65a84f87f267226d628c57b2b94a5d1437222
lucas <lucas.czeck@inteliger.com.br>
2023-02-08 09:22:35 -0300

    Desenvolvimento de view para login e alteração na página de login, cadastro será separado

--- home/views.py
+++ home/views.py
@@ -1,13 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-
-import BO.home.home
+from django.contrib.auth import authenticate, login
 
 
 class Login(View):
 
     def get(self, *args, **kwargs):
-        context = {}
+        return render(self.request, 'login.html')
 
-        return render(self.request, "login.html", context=context)
+    def post(self, *args, **kwargs):
+
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            # Redirecionar o usuário para a página desejada
+            return redirect('home')
+        else:
+            # Mostrar uma mensagem de erro
+            return HttpResponse("Nome de usuário ou senha inválidos.")
